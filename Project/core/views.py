c1137646c7dc8d425815a4ed12110780912b99c3
Soufiane-Majdar <soufianemjr3145@gmail.com>
2024-06-30 13:30:26 +0100

    new commit

--- Project/core/views.py
+++ Project/core/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.db.models import Q
-
+from django.core.paginator import Paginator
 
 def home(request):
     query = request.GET.get('q')
     categorie = request.GET.get('categorie')
+    page_number = request.GET.get('page', 1)
 
     publications = Publication.objects.all()
     thematiques = Thematique.objects.all()
@@ -21,20 +22,34 @@ def home(request):
     if categorie:
         publications = publications.filter(thematiques__nom__exact=categorie)
 
-    context = {
-        'publications': publications,
-        'thematiques':thematiques
+    paginator = Paginator(publications, 10)  # Show 10 publications per page
+    page_obj = paginator.get_page(page_number)
 
+    context = {
+        'publications': page_obj,
+        'thematiques': thematiques
     }
     return render(request, 'home.html', context)
 
-
-def about(request,id):
+def about(request, id):
     publication = Publication.objects.get(id=id)
     thematiques = Thematique.objects.all()
 
+    if request.method == "POST":
+        contenu = request.POST.get('contenu')
+        utilisateur_id = request.POST.get('utilisateur_id')  # Assuming you are getting the user ID from the form
+
+        if contenu and utilisateur_id:
+            utilisateur = Utilisateur.objects.get(id=utilisateur_id)
+            Commentaire.objects.create(contenu=contenu, publication=publication, utilisateur=utilisateur)
+
+            return redirect('about', id=id)
+
+    commentaires = Commentaire.objects.filter(publication=publication)
+
     context = {
         'publication': publication,
-        'thematiques':thematiques
+        'thematiques': thematiques,
+        'commentaires': commentaires
     }
-    return render(request, 'about.html',context)
+    return render(request, 'about.html', context)
